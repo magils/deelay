b6efe56beca2c7772bead4a1dc3947481c6e5fb3
Moises Gil <moisesgilsantana@gmail.com>
2020-10-22 13:38:42 -0400

    Improved server script

--- index.js
+++ index.js
@@ -2,70 +2,83 @@ const https = require('https')
 const throttle = require('@sitespeed.io/throttle')
 
 module.exports = (request, response, stdout) => {
-  const path = request.url.split('/');
-  const delay = path[1];
-  let redirectUrl = path.slice(2).join('/');
-  var dataArray = [];
 
+  try {
 
-  if (request.method === 'GET' && path.length > 2 && path[2] === "slow-connection") {
+      const path = request.url.split('/');
+      var dataArray = [];
 
-    redirectUrl = path.slice(3).join('/');
-    console.log("URL: ", redirectUrl);
+      if (request.method === 'GET' && path.length > 2 && path[2] === "slow-connection") {
 
-    throttle.start({up: 100, down: 100, rtt: 300}).then(() =>{
+        redirectUrl = path.slice(3).join('/');
+        console.log("Slow connection...");
 
-      const req = https.get(redirectUrl, (res) => {
-        res.on('data', (chunk) => { dataArray.push(chunk) });
-        res.on('end', () => {
-          var buffer = Buffer.concat(dataArray);
-          response.end(buffer);
-          throttle.stop();
+        throttle.start({up: 100, down: 100, rtt: 300}).then(() =>{
 
-          //Hack: Throttle doesn't restore the Network Interface speed when stops, setting high connection speed to not slowdown the server bandwidth
-          //Using "fois" profile values. see :  https://github.com/sitespeedio/throttle 
-          throttle.start({up: 5000, down: 20000, rtt: 2}).then(() => {throttle.stop()});
+          const req = https.get(redirectUrl, (res) => {
+            res.on('data', (chunk) => { dataArray.push(chunk) });
+            res.on('end', () => {
+              var buffer = Buffer.concat(dataArray);
+              response.end(buffer);
+              throttle.stop();
+            });
+          });
         });
-      });
-    });
 
- }  else if (request.method === 'GET' && !isNaN(delay) && path.length > 2) {
-    if (!redirectUrl.match(/^(http|https):/)) {
-      redirectUrl = `https://${redirectUrl}`;
-    }
-    stdout && stdout.write(`${delay}... `);
+    }  
+    else if (request.method === 'GET' && path.length > 2) {
 
-    setTimeout(() => {
-      stdout && stdout.write(`${redirectUrl}\n`);
-      response.statusCode = 302;
-      response.setHeader('Location', redirectUrl);
-      response.setHeader('Access-Control-Allow-Origin', '*');
+        const delay = path[1];
+        let redirectUrl = path.slice(2).join('/');
+
+        if (isNaN(delay)){
+          response.statusCode = 400;
+          response.end({"error": "Invalid timeout value"});
+        }
+
+        if (!redirectUrl.match(/^(http|https):/)) {
+          redirectUrl = `https://${redirectUrl}`;
+        }
+        stdout && stdout.write(`${delay}... `);
+
+        setTimeout(() => {
+          stdout && stdout.write(`${redirectUrl}\n`);
+          response.statusCode = 302;
+          response.setHeader('Location', redirectUrl);
+          response.setHeader('Access-Control-Allow-Origin', '*');
+          response.end();
+        }, delay);
+      } else if (request.method === 'OPTIONS') {
+        response.statusCode = 200;
+        response.setHeader('Access-Control-Allow-Origin', '*');
+        response.setHeader('Access-Control-Allow-Headers', request.headers['access-control-request-headers'] || '');
+        response.setHeader('Access-Control-Allow-Methods', request.headers['access-control-request-methods'] || '');
+        response.end();
+      } else if (request.method === 'HEAD'){
+
+          if(path[2] === "slow-connection") {
+            redirectUrl = path.slice(3).join('/');
+          }
+
+          const req = https.request(redirectUrl, {"method": "HEAD"}, (res) => {
+          response_headers = res.headers;
+          for (const key in response_headers) {
+            response.setHeader(key, response_headers[key]);
+          }
+          response.end();
+        });
+
+        req.end();
+    } else if (request.method == "POST" && path.length > 2 &&  path[2] == "/bandwidth/restore") {
+        throttle.start({up: 5000, down: 20000, rtt: 2}).then(() => {throttle.stop()});
+        response.statusCode = 201;
+        response.end()
+    } else {
+      response.statusCode = 404;
       response.end();
-    }, delay);
-  } else if (request.method === 'OPTIONS') {
-    response.statusCode = 200;
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    response.setHeader('Access-Control-Allow-Headers', request.headers['access-control-request-headers'] || '');
-    response.setHeader('Access-Control-Allow-Methods', request.headers['access-control-request-methods'] || '');
-    response.end();
-  } else if (request.method === 'HEAD'){
-
-    if(path[2] === "slow-connection") {
-      redirectUrl = path.slice(3).join('/');
     }
 
-      const req = https.request(redirectUrl, {"method": "HEAD"}, (res) => {
-      response_headers = res.headers;
-      for (const key in response_headers) {
-        response.setHeader(key, response_headers[key]);
-      }
-      response.end();
-    });
-
-    req.end();
-  }
-  else {
-    response.statusCode = 404;
-    response.end();
+  }catch(error){
+    console.log(error)
   }
 };
